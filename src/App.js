fa4fd6143b74e47305be3754f533de820363c152
Jack Bennett <franc-bennett@protonmail.com>
2018-08-24 15:14:19 +1000

    Canvas Rendering To Matrix
    Having some troubles displaying the whole image in some cases. Need to account for the difference between canvas size and matrix size.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import DisplayMatrix from './DisplayMatrix'
 
@@ -8,13 +8,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <DisplayMatrix width={80} height={30} />
+        <DisplayMatrix source={logo} width={100} height={100} />
       </div>
     );
   }
--- src/DisplayMatrix.js
+++ src/DisplayMatrix.js
@@ -5,30 +5,65 @@ class DisplayMatrix extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pixels: Array(props.width*props.height),
+            pixels: [],
             width: props.width,
             height: props.height,
+            content: document.createElement("img"),
+            source: props.source,
         };
+        
+        this.loadImagePixels();
+
+        // TODO: CHECK HOW TO INTEGRATE DIFFERENT TYPES OF INPUTS THROUGH THE PROPS
     }
     render() {
         var displayMatrix = [];
-        for (var index = 0; index < this.state.height; index++) {
-            for (var index2 = 0; index2 < this.state.width; index2++) {
-                displayMatrix.push(Pixel());
+        for (var index = 0; index < this.state.pixels.length; index++) {
+            if (index % this.state.width == 0) {
+                displayMatrix.push(<br />);
             }
-            displayMatrix.push(<br />);
+            displayMatrix.push(Pixel(this.state.pixels[index]));
         }
-
         return (displayMatrix);
     }
 
-    updatePixels() {
-    }
-}
+    loadImagePixels() {
+        var canvas = document.createElement("canvas");
+        var context = canvas.getContext('2d');
+        this.state.content.setAttribute("src", this.state.source);
+        this.state.content.setAttribute("width", "100");
+        this.state.content.setAttribute("height", "100");
+        
+        canvas.height = 100;
+        canvas.width = 100;
 
-function Pixel(props) {
+        document.body.appendChild(this.state.content);
+        document.body.appendChild(canvas);
+        context.fillRect(0, 0, 10, 10);
+        let frame = context.getImageData(0, 0, 100, 100);
+        for (var index = 0; index < (this.state.width*this.state.height); index += 4){
+            this.state.pixels.push([
+                frame.data[index],      // Red
+                frame.data[index+1],    // Green
+                frame.data[index+2],    // Blue
+                frame.data[index+3]     // Alpha
+            ]);
+        }
+
+    }
+    
+}    
+
+function Pixel(rgba) {
     return (
-        <button className="pixel" style={{backgroundColor: "#4CAF50",}}>
+        <button className="pixel" style={{backgroundColor: ('rgba(' +
+                                                            rgba[0] + ', ' +
+                                                            rgba[1] + ', ' +
+                                                            rgba[2] + ', ' +
+                                                            rgba[3]
+                                                            )
+                                        }}
+        >
         </button>
         );
 }
